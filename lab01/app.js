3594c0f7b6e49faabdba971e5279613af78caa40
madamski9 <maciek.adamski9@gmail.com>
2024-11-27 17:27:30 +0100

    lab01

--- lab01/app.js
+++ lab01/app.js
@@ -1,7 +1,32 @@
 async function fetchPokemonList() {
     try {
-        
+        const response = await fetch("https://pokeapi.co/api/v2/pokemon?limit=20&offset=0")
+        const data = await response.json()
+        return data.results
     } catch(error) {
-
+        console.error(error)
     }
 }
+
+async function fetchPokemonDetails(pokemonName) {
+    try {
+        const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`)
+        const data = await response.json()
+        return data
+    } catch(error) {
+        console.error(error)
+    }
+}
+
+document.addEventListener('DOMContentLoaded', async () => {
+    const pokemonListElement = document.getElementById('pokemon-list');
+
+    const pokemonList = await fetchPokemonList();
+    if (pokemonList) {
+        pokemonList.forEach(pokemon => {
+            const listItem = document.createElement('li');
+            listItem.textContent = pokemon.name;
+            pokemonListElement.appendChild(listItem);
+        });
+    }
+});
