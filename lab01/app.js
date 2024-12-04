457d252e25fe74a1a58cb0322c600822cfc4d8bf
madamski9 <maciek.adamski9@gmail.com>
2024-12-04 16:51:07 +0100

    lab01

--- lab01/app.js
+++ lab01/app.js
@@ -18,15 +18,33 @@ async function fetchPokemonDetails(pokemonName) {
     }
 }
 
-document.addEventListener('DOMContentLoaded', async () => {
+async function initPokemonList() {
     const pokemonListElement = document.getElementById('pokemon-list');
-
+    const pokemonDetailsElement = document.getElementById('pokemon-details');
+    const searchInput = document.querySelector('.search');
     const pokemonList = await fetchPokemonList();
     if (pokemonList) {
         pokemonList.forEach(pokemon => {
             const listItem = document.createElement('li');
             listItem.textContent = pokemon.name;
+            listItem.classList.add('clickable');
+            listItem.addEventListener('click', async () => {
+                const pokemonDetails = await fetchPokemonDetails(pokemon.name);
+                displayPokemonDetails(pokemonDetails, pokemonDetailsElement);
+            });
             pokemonListElement.appendChild(listItem);
         });
     }
-});
+}
+
+function displayPokemonDetails(details, element) {
+    element.innerHTML = `
+        <h4>${details.name}</h4>
+        <img src="${details.sprites.front_default}" alt="${details.name}">
+        <p>Height: ${details.height}</p>
+        <p>Weight: ${details.weight}</p>
+        <p>Type: ${details.types.map(typeInfo => typeInfo.type.name).join(', ')}</p>
+    `;
+}
+
+initPokemonList();
